d6f6b043f6ad247e1b2d1d233bc2c8874e43c166
whackamole7 <whackamole7@proton.me>
2025-10-13 15:38:39 +0300

    Updates

---
+++ assets/arrow-left-ky2MKHIn.js
@@ -0,0 +1,14 @@
+import { m as b } from "./core-KOU3-1-0.js";
+import "./index-CmVjgkl9.js";
+import "./index.es-R16-kKPw.js";
+const arrowLeftSvg = b`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M13 7.99a1 1 0 0 1-1 1H4.4l2.46 2.46a1 1 0 1 1-1.41 1.41L1.29 8.7a1 1 0 0 1 0-1.41L5.46 3.1a1 1 0 0 1 1.41 1.42L4.41 6.99H12a1 1 0 0 1 1 1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;
+export {
+  arrowLeftSvg
+};
