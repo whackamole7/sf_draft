665c1bac35bf20fbde6b45fc53abab004b6ff2ee
whackamole7 <whackamole7@proton.me>
2025-10-13 19:31:35 +0300

    Updates

--- assets/cursor-DYGejS7q.js
+++
@@ -1,9 +0,0 @@
-import { m as b } from "./core-DDmdm4RN.js";
-import "./index-B2UAMT85.js";
-import "./index.es-CU7ypKDA.js";
-const cursorSvg = b` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;
-export {
-  cursorSvg
-};
