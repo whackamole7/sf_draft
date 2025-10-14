bcb7180195e760ae1d3f89ee1bcf398058b16224
whackamole7 <whackamole7@proton.me>
2025-10-14 14:15:01 +0300

    Updates

---
+++ assets/chevron-left-D85n_ukk.js
@@ -0,0 +1,14 @@
+import { m as b } from "./core-C0nGSvGk.js";
+import "./index-JICGhNQ3.js";
+import "./index.es-Bkf9_DRc.js";
+const chevronLeftSvg = b`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;
+export {
+  chevronLeftSvg
+};
