ac0ac8b4244adadc0dd1abe55021a2f7e7430a7d
whackamole7 <whackamole7@proton.me>
2025-10-15 14:49:05 +0300

    Updates

---
+++ assets/all-wallets-BJxwyB-i.js
@@ -0,0 +1,12 @@
+import { m as b } from "./core-CX15mqxm.js";
+import "./index-C8dDjnXG.js";
+import "./index.es-DucaWqq1.js";
+const allWalletsSvg = b`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;
+export {
+  allWalletsSvg
+};
---
+++ assets/cursor-BbnRr4On.js
@@ -0,0 +1,9 @@
+import { m as b } from "./core-CX15mqxm.js";
+import "./index-C8dDjnXG.js";
+import "./index.es-DucaWqq1.js";
+const cursorSvg = b` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;
+export {
+  cursorSvg
+};
