5726322fad6a7c301ad7f0e59c4c181169b19f80
whackamole7 <whackamole7@proton.me>
2025-10-15 12:48:26 +0300

    Updates

---
+++ assets/cursor-Cv9T5Vvu.js
@@ -0,0 +1,9 @@
+import { m as b } from "./core-DPzP4GO4.js";
+import "./index-CnxZN79n.js";
+import "./index.es-DgYFHRAo.js";
+const cursorSvg = b` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;
+export {
+  cursorSvg
+};
