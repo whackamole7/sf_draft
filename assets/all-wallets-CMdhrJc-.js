4adf5b85fa3c569ed0253e85dafc018e4facbed9
whackamole7 <whackamole7@proton.me>
2025-10-28 13:00:15 +0300

    Updates

---
+++ assets/all-wallets-CMdhrJc-.js
@@ -0,0 +1,12 @@
+import { m as b } from "./core-eDlGvWZN.js";
+import "./index-DCbt1CxP.js";
+import "./index.es-ELa2lgSA.js";
+const allWalletsSvg = b`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;
+export {
+  allWalletsSvg
+};
