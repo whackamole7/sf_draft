9a7f8e1526e02a82b76a452b61891f470c3965ae
whackamole7 <whackamole7@proton.me>
2026-01-19 13:10:54 +0300

    Updates

---
+++ assets/chevron-right-CVaVqt0n.js
@@ -0,0 +1,14 @@
+import { m as b } from "./core-Ch1w-EA8.js";
+import "./index-C-Y1mjzp.js";
+import "./index.es-D4U2rwMu.js";
+const chevronRightSvg = b`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;
+export {
+  chevronRightSvg
+};
