72eb124d902205986c9cfcfd76f0bb9f4f89685e
whackamole7 <whackamole7@proton.me>
2026-02-19 13:29:46 +0300

    Updates

--- assets/cursor-VxPl-b7C.js
+++
@@ -1,9 +0,0 @@
-import { m as b } from "./core-ClRi3g_9.js";
-import "./index-BI3hgPYP.js";
-import "./index.es-CRJ_kC7R.js";
-const cursorSvg = b` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;
-export {
-  cursorSvg
-};
